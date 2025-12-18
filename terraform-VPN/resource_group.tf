f11a7934c85df9adbde01c84e306145d0ed46db0
Nagaraju Kuruva <nagaraju.kuruva@in.techwave.net>
2025-12-18 15:16:24 +0530

    azure vpnnnn

--- terraform-VPN/resource_group.tf
+++ terraform-VPN/resource_group.tf
@@ -2,6 +2,8 @@ provider "azurerm" {
   features {
     
   }
+  alias = "provider2-resource"
+  subscription_id = "1349db56-7a42-446d-bce1-63520d61eccf"
 }
 
 resource "azurerm_resource_group" "myrg" {
