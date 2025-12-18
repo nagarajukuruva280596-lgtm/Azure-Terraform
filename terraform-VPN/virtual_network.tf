95a9a872fc4d337505c2f059192c70b7cc525033
Nagaraju Kuruva <nagaraju.kuruva@in.techwave.net>
2025-12-18 15:11:46 +0530

    azure vpnn

--- terraform-VPN/virtual_network.tf
+++ terraform-VPN/virtual_network.tf
@@ -8,13 +8,13 @@ resource "azurerm_virtual_network" "myvpn" {
 resource "azurerm_subnet" "mysubnet" {
     name = "subnet1"
     resource_group_name = azurerm_resource_group.myrg.name
-    virtual_network_name = azurerm_virtual_network.myvpn.name 
+    virtual_network_name = azurerm_virtual_network.myvpn.name
+    address_prefixes = [ "10.0.0.0/32"]
 }
 
 data "azurerm_public_ip" "pub-ip" {
     name = "pub-ip"
     resource_group_name = azurerm_resource_group.myrg.name
-    location = azurerm_resource_group.myrg.location
 }
 
 resource "azurerm_network_interface" "mynic" {
@@ -25,6 +25,7 @@ resource "azurerm_network_interface" "mynic" {
     name = "internal"
     subnet_id = azurerm_subnet.mysubnet.id
     private_ip_address_allocation = "static"
+    public_ip_address_id = data.azurerm_public_ip.pub-ip.id
   }
 }
 
