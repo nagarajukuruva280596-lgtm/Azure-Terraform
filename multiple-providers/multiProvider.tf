e2c03a74d24f156b0303a3edc2cb1b83f52c2a84
Nagaraju Kuruva <nagaraju.kuruva@in.techwave.net>
2025-12-18 12:45:56 +0530

    addes storgae accounttt

--- multiple-providers/multiProvider.tf
+++ multiple-providers/multiProvider.tf
@@ -5,6 +5,10 @@ terraform {
       source = "hashicorp/azurerm"
       version = "4.56.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
   }
 }
 
@@ -22,6 +26,12 @@ provider "azurerm" {
   subscription_id = "1349db56-7a42-446d-bce1-63520d61eccf"
 }
 
+resource "random_string" "random_name" {
+  length = 16
+  upper = false
+  
+}
+
 resource "azurerm_resource_group" "myrg-1" {
   name = "myrg-1"
   location = "East US 2"
@@ -34,7 +44,7 @@ resource "azurerm_resource_group" "myrg-2" {
 }
 
 resource "azurerm_storage_account" "storage-acc" {
-  name                     = "storage_acc"
+  name                     = "mysz${random_string.random_name}"
   resource_group_name      = azurerm_resource_group.myrg-1.id
   location                 = azurerm_resource_group.myrg-1.location
   account_tier             = "Standard"
