2a9faaa2fa85046cc29933c8f35e14d9818f5f1c
Nagaraju Kuruva <nagaraju.kuruva@in.techwave.net>
2025-12-18 15:02:58 +0530

    azure vpn

---
+++ terraform-VPN/resource_group.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "myrg" {
+    location = "West US"
+    name = "myrg"
+}
---
+++ terraform-VPN/virtual_network.tf
@@ -0,0 +1,33 @@
+resource "azurerm_virtual_network" "myvpn" {
+  name = "vpn1"
+  address_space = [ "10.0.0.0/32"]
+  resource_group_name = azurerm_resource_group.myrg.name
+  location = azurerm_resource_group.myrg.location
+}
+
+resource "azurerm_subnet" "mysubnet" {
+    name = "subnet1"
+    resource_group_name = azurerm_resource_group.myrg.name
+    virtual_network_name = azurerm_virtual_network.myvpn.name 
+}
+
+data "azurerm_public_ip" "pub-ip" {
+    name = "pub-ip"
+    resource_group_name = azurerm_resource_group.myrg.name
+    location = azurerm_resource_group.myrg.location
+}
+
+resource "azurerm_network_interface" "mynic" {
+  name = "nic1"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location = azurerm_resource_group.myrg.location
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.mysubnet.id
+    private_ip_address_allocation = "static"
+  }
+}
+
+output "pubic_ip_add" {
+  value = data.azurerm_public_ip.pub-ip.name
+}
