65425741969ee5858235263c57d20d394084771c
Nagaraju Kuruva <nagaraju.kuruva@in.techwave.net>
2025-12-18 12:38:16 +0530

    addes storgae account

--- multiple-providers/multiProvider.tf
+++ multiple-providers/multiProvider.tf
@@ -32,3 +32,14 @@ resource "azurerm_resource_group" "myrg-2" {
   location = "West US"
   provider = azurerm.provider2-westus
 }
+
+resource "azurerm_storage_account" "storage_acc" {
+  name                     = "storage_acc"
+  resource_group_name      = azurerm_resource_group.myrg-1.id
+  location                 = azurerm_resource_group.myrg-1.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  tags = {
+    environment = "staging"
+  }
+}
