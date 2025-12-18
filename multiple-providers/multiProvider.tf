e87d08eda3edfa20549d921419b5253a765b8a58
Nagaraju Kuruva <nagaraju.kuruva@in.techwave.net>
2025-12-18 11:30:17 +0530

    added multi providerss

--- multiple-providers/multiProvider.tf
+++ multiple-providers/multiProvider.tf
@@ -12,12 +12,14 @@ provider "azurerm" {
     features {
       
     }
+    subscription_id = "1349db56-7a42-446d-bce1-63520d61eccf"
 }
 
 provider "azurerm" {
   features {
   }
   alias = "provider2-westus"
+  subscription_id = "1349db56-7a42-446d-bce1-63520d61eccf"
 }
 
 resource "azurerm_resource_group" "myrg-1" {
